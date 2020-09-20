80433eae985a6287c4aedd6158f1a2ca13807b15
C01dy <faltrue7@gmail.com>
2020-09-20 23:27:15 +0300

    небольшие фиксы

--- scripts/select.js
+++ scripts/select.js
@@ -1,7 +1,6 @@
 const input = document.querySelector('.input-field input');
 const collection = document.querySelector('.collection .collection-items');
 const addBtn = document.querySelector('.input-field div a');
-// const inputForm = document.querySelector('.input-field');
 
 class Todos {
   get id() {
@@ -9,25 +8,31 @@ class Todos {
   }
 
   render() {
-    const todos = JSON.parse(localStorage.getItem('todos'));
+    if (localStorage.length) {
+      const todos = JSON.parse(localStorage.getItem('todos'));
 
-    const todosDOMElement = todos
-      .map((todo) => {
-        return `
-        <li class="collection-item" id=${todo.id}>
+      const todosDOMElement = todos
+        .map((todo) => {
+          return `
+        <li class="collection-item" id=${todo.id} style='text-decoration: ${
+            todo.isCompleted ? 'line-through' : 'none'
+          }'>
             ${todo.text}
           <form href="#!" class="secondary-content">
           <label>
-            <input type="checkbox" class="filled-in" ${todo.isCompleted ? 'checked' : ''}/>
+            <input type="checkbox" class="filled-in" ${
+              todo.isCompleted ? 'checked' : ''
+            }/>
             <span></span>
           </label>
           </form>
         </li>
         `;
-      })
-      .join('');
+        })
+        .join('');
 
-    collection.innerHTML = todosDOMElement;
+      collection.innerHTML = todosDOMElement;
+    }
   }
 
   createTodo(id, text, isCompleted) {
@@ -47,26 +52,30 @@ class Todos {
         JSON.stringify([...todos, this.createTodo(this.id, input.value, false)])
       );
     } else {
-      alert('Put something')
+      alert('Put something');
     }
     this.render();
   }
 
   toggleComplete() {
-    document.body.addEventListener("click", e => {
+    document.body.addEventListener('click', (e) => {
       if (e.target.classList.contains('filled-in')) {
         const id = e.path[3].id;
         const todos = JSON.parse(localStorage.getItem('todos'));
-        const itemIdx = todos.findIndex(item => item.id === id);
-        const item = todos[itemIdx]
-        
-        localStorage.setItem('todos', JSON.stringify([
-          ...todos.slice(0, itemIdx),
-          this.createTodo(item.id, item.text, !item.isCompleted),
-          ...todos.slice(itemIdx + 1)
-        ]))
+        const itemIdx = todos.findIndex((item) => item.id === id);
+        const item = todos[itemIdx];
+
+        localStorage.setItem(
+          'todos',
+          JSON.stringify([
+            ...todos.slice(0, itemIdx),
+            this.createTodo(item.id, item.text, !item.isCompleted),
+            ...todos.slice(itemIdx + 1),
+          ])
+        );
+        this.render();
       }
-    })
+    });
   }
 }
 
